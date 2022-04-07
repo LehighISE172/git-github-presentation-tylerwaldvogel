7de0952378947aae8247f182e663fb1114a42d33
tylerwaldvogel <89604408+tylerwaldvogel@users.noreply.github.com>
2022-04-07 11:24:01 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -18,9 +18,13 @@ g.addEdge(2,3)
 
 
 t = turtle.Turtle()
+t1 = turtle.Turtle()
+t2 = turtle.Turtle()
 ws = turtle.Screen()
-
+t2.pensize(10)
+t2.color('green')
 r = 30
+
 t.up()
 t.goto(-200, -200)
 t.down()
@@ -29,17 +33,49 @@ t.color('red')
 t.circle(r)
 t.end_fill()
 
+t1.up()
+t1.goto(-250, -250)
+t1.color('black')
+t1.write('Driver', font=('Arial', 20))
+t1.down()
+
+t2.up()
+t2.goto(-200, -140)
+t2.down()
+t2.goto(0, 150)
+
+t.up()
 t.goto(0, 150)
+t.down()
 t.begin_fill()
-t.color('green')
+t.color('orange')
 t.circle(r)
 t.end_fill()
 
+t2.goto(200, -140)
+t1.up()
+t1.goto(-60, 250)
+t1.color('black')
+t1.write('Restaurant', font=('Arial', 20))
+t1.down()
+
+t.up()
 t.goto(200, -200)
+t.down()
 t.begin_fill()
 t.color('yellow')
 t.circle(r)
 t.end_fill()
+
+t1.up()
+t1.goto(175, -250)
+t1.color('black')
+t1.write('Customer', font=('Arial', 20))
+t1.down()
+
+
 ws.exitonclick()
+
+
 
 
